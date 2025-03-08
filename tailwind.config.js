ba39117f8f1a458cb18d999cc574bc352b1b5aab
GXCHINA <38310293+GXCHINA@users.noreply.github.com>
2025-03-08 23:33:08 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      aspectRatio: {
+        'w-16': 16,
+        'h-9': 9,
+      },
+    },
+  },
+  plugins: [],
+} 
